40261178ea7fc6e448b868d851499467a5db5ef9
GFM-Austinus-devotus <gfreitasmelo67@gmail.com>
2025-10-21 10:38:06 -0300

    Training some situations with Elif

---
+++ Fundamentos/17-Elif.py
@@ -0,0 +1,19 @@
+# Calculadora
+
+num1 = float(input("Digite o primeiro número\n"))
+num2 = float(input("Digite o segundo número\n"))
+operation = input("Digite a operção a realizar (+ - * /)\n")
+
+if operation == "+":
+    result = num1 + num2
+elif operation == "-":
+    result = num1 - num2
+elif operation == "*":
+    result = num1 * num2
+elif operation == "/":
+    result = num1 / num2
+else:
+    print("Operação Inválida")
+    result = 0
+
+print(f"Resultado é: {result:.2f}")
